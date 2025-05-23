91031afdb3bfabac7a59e7b196456a76f14b7da4
Jadavnitin <nitinkumarjadav83@gmail.com>
2025-05-23 17:48:27 +0530

    Your commit message here

--- src/components/TableOfProduct.jsx
+++ src/components/TableOfProduct.jsx
@@ -165,7 +165,7 @@ const TableOfProduct = ({ externalData = [] }) => {
   return (
     <Container>
       <TableWrapper>
-        <div className='table-div'>
+      
         <StyledTable>
           <thead>
             <tr>
@@ -227,7 +227,7 @@ const TableOfProduct = ({ externalData = [] }) => {
             )}
           </tbody>
         </StyledTable>
-        </div>
+        
         <Footer>
           <PaginationContainer>{renderPagination()}</PaginationContainer>
           <PageSizeWrapper>
@@ -326,10 +326,10 @@ const TableWrapper = styled.div`
   }
 
   
-  .table-div{
+  /* .table-div{
     max-height:350px;
     overflow-y: scroll;
-  }
+  } */
 
 `;
 
